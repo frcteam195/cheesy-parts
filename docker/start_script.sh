f2d1f4fdcc0b88720eae4637a8c27e6e4ded7455
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-09-28 08:22:03 +0000

    Update scripts

--- docker/start_script.sh
+++ docker/start_script.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+
+if [ ! -f /var/lib/mysql/.initialized_db ]
+then
+    echo "Database not found. Initializing"
+    cp -R /var/lib/mysql_initial/. /var/lib/mysql
+    touch /var/lib/mysql/.initialized_db
+    chown -R mysql:mysql /var/lib/mysql
+fi
+
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
 cd /cheesy-parts
 source /root/rbenv.env
 sleep 4
-ruby parts_server_wrapper.rb
+ruby parts_server_wrapper.rb &
+
+child=$!
+wait "$child"
 
